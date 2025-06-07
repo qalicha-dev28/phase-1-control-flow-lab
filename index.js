0eab39bc95b44f9d7118356e8c38912952fc0bc0
qalicha-dev28 <qalichanajma44@gmail.com>
2025-06-07 10:26:15 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(riderDistance){
+  if (riderDistance <= 400) {
+    return 'This one is on me!';
+  }
+  else if (riderDistance > 400 && riderDistance <= 2000) {
+    return 'That will be twenty bucks.';
+  } 
+  else if (riderDistance > 2000 && riderDistance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } 
+  else {
+    return 'No can do.';
+  } 
+
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
